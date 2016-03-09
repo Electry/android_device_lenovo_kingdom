9db1cd77cf107120ccd020defcdf0ca9722f538d
Jan Dula <frantisheq@gmail.com>
2016-03-09 23:21:51 +0100

    Revert "use camera wrapper from samsung trlte"
    This reverts commit 7845811ad5590a5c586c09ad0bfc74f2f0f4f059.

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,23 +1,15 @@
-ifeq ($(TARGET_PROVIDES_CAMERA_HAL),true)
-
 LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := \
-    CameraWrapper.cpp
-
-LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libutils libbinder
+LOCAL_MODULE := camera.msm8974
+LOCAL_SRC_FILES := CameraWrapper.cpp
 
 LOCAL_C_INCLUDES := \
     system/media/camera/include
 
-LOCAL_LDLIBS += -ldl  
+LOCAL_SHARED_LIBRARIES := \
+    libhardware liblog libcamera_client libutils libcutils libdl
 
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-LOCAL_MODULE := camera.msm8974
-LOCAL_MODULE_TAGS := optional
-
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/hw
+LOCAL_CFLAGS := -Werror
 include $(BUILD_SHARED_LIBRARY)
-
-endif
