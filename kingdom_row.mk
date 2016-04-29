e15a3220e5d8b798553140351299075fcff724a0
Jan Dula <frantisheq@gmail.com>
2016-04-30 00:14:45 +0200

    enable FM radio

--- kingdom_row.mk
+++ kingdom_row.mk
@@ -123,11 +123,10 @@ PRODUCT_PACKAGES += \
     gps.msm8974
 
 # FM
-#PRODUCT_PACKAGES += \
-#    FM2 \
-#    FMRecord \
-#    libqcomfm_jni \
-#    qcom.fmradio
+PRODUCT_PACKAGES += \
+    FM2 \
+    libqcomfm_jni \
+    qcom.fmradio
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf \
