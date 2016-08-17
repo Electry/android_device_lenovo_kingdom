4034ee2b86b8b96a36de756205e8b37ee15807ec
Michal Chvíla <electrydev@gmail.com>
2016-08-18 01:27:00 +0200

    camera: Clean CameraWrapper.cpp
    Change-Id: I3d7cade0ce0423eba4d94ff373c76f2316aa39cb
    Signed-off-by: Michal Chvíla <electrydev@gmail.com>

--- camera/CameraWrapper.cpp
+++ camera/CameraWrapper.cpp
@@ -80,7 +80,6 @@ typedef struct wrapper_camera_device {
     camera_device_t base;
     int id;
     camera_device_t *vendor;
-    bool initial_get;
 } wrapper_camera_device_t;
 
 #define VENDOR_CALL(device, func, ...) ({ \
@@ -224,13 +223,6 @@ static int camera_store_meta_data_in_buffers(struct camera_device *device,
     return VENDOR_CALL(device, store_meta_data_in_buffers, enable);
 }
 
-static bool is4k(CameraParameters2 &params) {
-    int video_width, video_height;
-    params.getVideoSize(&video_width, &video_height);
-
-    return video_width*video_height > 1920*1080;
-}
-
 static int camera_start_recording(struct camera_device *device)
 {
     ALOGV("%s->%08X->%08X", __FUNCTION__, (uintptr_t)device,
@@ -238,23 +230,6 @@ static int camera_start_recording(struct camera_device *device)
 
     if (!device)
         return EINVAL;
-
-
-    CameraParameters2 parameters;
-    parameters.unflatten(String8(camera_get_parameters(device)));
-    if (CAMERA_ID(device) == BACK_CAMERA_ID) {
-        if (is4k(parameters)) {
-            parameters.set("preview-format", "nv12-venus");
-        }
-        parameters.set("picture-size", "4160x3120");
-    }
-    camera_set_parameters(device,  strdup(parameters.flatten().string()));
-
-    CameraParameters2 parameters2;
-    parameters2.unflatten(String8(VENDOR_CALL(device, get_parameters)));
-    parameters2.dump();
-
-
 
     return VENDOR_CALL(device, start_recording);
 }
@@ -347,23 +322,7 @@ static int camera_set_parameters(struct camera_device *device,
     if (!device)
         return -EINVAL;
 
-	const char *tmpParams = strdup(parameters);
-
-    CameraParameters2 params;
-	params.unflatten(String8(tmpParams));
-
-    gClearImageEnabled = strcmp(params.get("clear-image"), "on") == 0;
-    if (gClearImageEnabled) {
-        params.set("high-resolution", "1300");
-        params.set("superzoom", "0");
-    } else {
-        params.set("high-resolution", "0");
-        params.remove("superzoom");
-    }
-
-    delete tmpParams;
-
-    return VENDOR_CALL(device, set_parameters, strdup(params.flatten().string()));
+    return VENDOR_CALL(device, set_parameters, strdup(parameters));
 }
 
 static char *camera_get_parameters(struct camera_device *device)
@@ -374,62 +333,7 @@ static char *camera_get_parameters(struct camera_device *device)
     if (!device)
         return NULL;
 
-    char *parameters = VENDOR_CALL(device, get_parameters); 
-    wrapper_camera_device_t *wrapper = (wrapper_camera_device_t *)device;
-
-    if (wrapper->initial_get) {
-        wrapper->initial_get = false;
-        CameraParameters2 params;
-        params.unflatten(String8(parameters));
-        params.set("cyanogen-camera", "1");
-        VENDOR_CALL(device, set_parameters, strdup(params.flatten().string())); 
-        parameters = VENDOR_CALL(device, get_parameters);
-    }
-
-    CameraParameters2 params;
-    params.unflatten(String8(parameters));
-    if (CAMERA_ID(device) == BACK_CAMERA_ID) {
-        /* Disable 352x288 preview sizes, the combination of this preview size and larger resolutions stalls the HAL */
-        params.set(CameraParameters::KEY_SUPPORTED_PREVIEW_SIZES,
-            "3840x2160,2560x1440,1920x1440,1920x1080,1440x1080,1280x720,960x720,960x540,864x480,800x480,768x432,720x480,640x480,640x360,576x432,480x320,384x288,320x240,240x160,176x144");
-        params.set(CameraParameters::KEY_SUPPORTED_VIDEO_SIZES,
-            "3840x2160,1920x1080,1280x720,864x480,800x480,720x480,640x480,320x240,176x144");
-        /* Remove 5328x2997 to get 13mpix with HDR */
-        params.set(CameraParameters::KEY_SUPPORTED_PICTURE_SIZES,
-            "5328x2997,4864x2736,4208x3000,4000x3000,4096x2160,3840x2160,3664x2748,4224x2376,3840x2160,3280x2460,3200x2400,2832x2124,3264x1836,3200x1800,2592x1944,2560x1440,2048x1536,1920x1440,1920x1080,1600x1200,1440x1080,1280x768,1280x720,1024x768,800x600,800x480,720x480,640x480,320x240,176x144");
-        params.set("preview-fps-range-values", "(7500,30000),(8000,30000),(30000,30000)");
-        params.set("supported-live-snapshot-sizes",
-            "4000x3000,3200x2400,2592x1944,2560x1440,2048x1536,1920x1440,1920x1080,1600x1200,1280x768,1280x720,1024x768,800x600,864x480,800x480,720x480,640x480,320x240,176x144");
-        params.set(CameraParameters::KEY_HORIZONTAL_VIEW_ANGLE, "60.0");
-        params.set("min-focus-pos-index", "0");
-        params.set("max-focus-pos-index", "300");
-    } else if (CAMERA_ID(device) == FRONT_CAMERA_ID) { 
-        /* Inject all supported resolutions */
-        params.set(CameraParameters::KEY_SUPPORTED_VIDEO_SIZES,
-            "1920x1080,1280x720,864x480,800x480,720x480,640x480,480x320,352x288,320x240,176x144");
-        params.set(CameraParameters::KEY_SUPPORTED_PREVIEW_SIZES,
-            "1920x1080,1440x1080,1280x960,1280x720,960x720,960x540,864x480,800x480,768x432,720x480,640x480,640x360,576x432,480x320,384x288,352x288,320x240,240x160,176x144");
-        params.set("preview-fps-range-values", "(7500,30000),(8000,30000),(30000,30000)");
-        if(strcmp(params.get(CameraParameters::KEY_PICTURE_SIZE), "352x288") == 0 ||
-           strcmp(params.get(CameraParameters::KEY_PICTURE_SIZE), "176x144") == 0) {
-            params.set(CameraParameters::KEY_HORIZONTAL_VIEW_ANGLE, "61.0");
-        } else {
-            params.set(CameraParameters::KEY_HORIZONTAL_VIEW_ANGLE, "65.5");
-        }
-    }
-
-    params.set("clear-image-values", "off,on");
-    params.set("clear-image", gClearImageEnabled ? "on" : "off");
-
-    const char *pf = params.get(android::CameraParameters::KEY_PREVIEW_FORMAT);
-    if (pf && strcmp(pf, "nv12-venus") == 0) {
-        params.set(android::CameraParameters::KEY_PREVIEW_FORMAT, "yuv420sp");
-    }
-
-    params.remove("high-resolution");
-    params.remove("superzoom");
-
-    return strdup(params.flatten().string());
+    return VENDOR_CALL(device, get_parameters);
 }
 
 static void camera_put_parameters(struct camera_device *device, char *params)
@@ -551,8 +455,6 @@ static int camera_device_open(const hw_module_t *module, const char *name,
         }
         memset(camera_device, 0, sizeof(*camera_device));
         camera_device->id = cameraid;
-
-        camera_device->initial_get = true;
 
         rv = gVendorModule->common.methods->open(
                 (const hw_module_t*)gVendorModule, name,
