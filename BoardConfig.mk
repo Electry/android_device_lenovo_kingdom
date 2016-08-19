3c7f4e1643d616499c0c77f6e285dc082d1673d7
Michal Chvíla <electrydev@gmail.com>
2016-08-19 18:17:50 +0200

    BoardConfig.mk: Update TARGET_KERNEL_CONFIG to k920
    Change-Id: Ida6e094adc5e9640dbb478f1fcef0733ace732c5
    Signed-off-by: Michal Chvíla <electrydev@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -49,7 +49,7 @@ BOARD_DTBTOOL_ARGS := -2
 TARGET_KERNEL_ARCH := arm
 BOARD_KERNEL_CMDLINE := console=tty60,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3b7 ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1 vmalloc=480M
 TARGET_KERNEL_SOURCE := kernel/lenovo/msm8974
-TARGET_KERNEL_CONFIG := cyanogenmod_kingdom_row_defconfig
+TARGET_KERNEL_CONFIG := cyanogenmod_k920_defconfig
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
 
 # Enable DIAG on debug builds
