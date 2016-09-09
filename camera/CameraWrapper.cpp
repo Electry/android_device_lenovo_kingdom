0eafe95d177679c89fbb17e3681ec2003c6365bc
Gabriele M <moto.falcon.git@gmail.com>
2016-09-09 17:35:26 +0200

    camera: Increase camera open retries
    Cameras can be opened only after the camera server has configured
    them. Since Android N the framework usually tries to access the
    cameras to determine whether they have a flashlight too early,
    causing the flashlight tile not to work. Try to open the camera
    a few times before giving up to prevent this.
    
    Note that mm-qcamera-daemon already tries to open the camera twice
    with 20ms of delay, so the total number of attempts is two times
    the one defined here.
    
    Change-Id: I664fdfdafe0829719fcd0a6a31057d761e4f07d9

--- camera/CameraWrapper.cpp
+++ camera/CameraWrapper.cpp
@@ -38,6 +38,9 @@
 #define BACK_CAMERA_ID 0
 #define FRONT_CAMERA_ID 1
 
+#define OPEN_RETRIES    10
+#define OPEN_RETRY_MSEC 40
+
 using namespace android;
 
 static Mutex gCameraWrapperLock;
@@ -466,9 +469,16 @@ static int camera_device_open(const hw_module_t *module, const char *name,
         memset(camera_device, 0, sizeof(*camera_device));
         camera_device->id = cameraid;
 
-        rv = gVendorModule->common.methods->open(
-                (const hw_module_t*)gVendorModule, name,
-                (hw_device_t**)&(camera_device->vendor));
+        int retries = OPEN_RETRIES;
+        bool retry;
+        do {
+            rv = gVendorModule->common.methods->open(
+                    (const hw_module_t*)gVendorModule, name,
+                    (hw_device_t**)&(camera_device->vendor));
+            retry = --retries > 0 && rv;
+            if (retry)
+                usleep(OPEN_RETRY_MSEC * 1000);
+        } while (retry);
         if (rv) {
             ALOGE("vendor camera open fail");
             goto fail;
