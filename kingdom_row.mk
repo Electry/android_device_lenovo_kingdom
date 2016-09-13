ce0636190c17f2b8cb8f37643eabd81c6c972ff2
frantisheq <frantisheq@gmail.com>
2016-09-13 19:54:56 +0200

    gps: Enable gps.msm8974 again and copy flp.conf

--- kingdom_row.mk
+++ kingdom_row.mk
@@ -120,8 +120,8 @@ PRODUCT_PACKAGES += \
     liboverlay
 
 # GPS
-#PRODUCT_PACKAGES += \
-#    gps.msm8974
+PRODUCT_PACKAGES += \
+    gps.msm8974
 
 # FM
 PRODUCT_PACKAGES += \
@@ -133,8 +133,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf \
     $(LOCAL_PATH)/gps/izat.conf:system/etc/izat.conf \
     $(LOCAL_PATH)/gps/quipc.conf:system/etc/quipc.conf \
-    $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf
-#    $(LOCAL_PATH)/gps/flp.conf:system/etc/flp.conf \
+    $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf \
+    $(LOCAL_PATH)/gps/flp.conf:system/etc/flp.conf
 
 # Lights
 PRODUCT_PACKAGES += \
