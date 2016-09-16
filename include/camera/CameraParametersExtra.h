43c147e32777f0ba8954f18944d4401a66207f2a
frantisheq <frantisheq@gmail.com>
2016-09-17 00:34:29 +0200

    camera: fix missing symbol

--- include/camera/CameraParametersExtra.h
+++ include/camera/CameraParametersExtra.h
@@ -15,7 +15,9 @@
  */
 
 #define CAMERA_PARAMETERS_EXTRA_C \
-    const char CameraParameters::FOCUS_MODE_MANUAL_POSITION[] = "manual";
+    const char CameraParameters::FOCUS_MODE_MANUAL_POSITION[] = "manual"; \
+    const char CameraParameters::WHITE_BALANCE_MANUAL_CCT[] = "manual-cct";
 
 #define CAMERA_PARAMETERS_EXTRA_H \
-    static const char FOCUS_MODE_MANUAL_POSITION[];
+    static const char FOCUS_MODE_MANUAL_POSITION[]; \
+    static const char WHITE_BALANCE_MANUAL_CCT[];
