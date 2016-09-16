769161165125e7eb48cc565bbab56d6176ac353c
Jan Dula <frantisheq@gmail.com>
2016-09-16 20:10:01 +0200

    Revert: bg_apps_limit: enable 32 running apps

--- kingdom_row.mk
+++ kingdom_row.mk
@@ -223,8 +223,7 @@ PRODUCT_PACKAGES += \
 
 # Performance
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.qualcomm.perf.cores_online=2 \
-    ro.sys.fw.bg_apps_limit=32
+    ro.qualcomm.perf.cores_online=2
 
 # Permissions
 PRODUCT_COPY_FILES += \
