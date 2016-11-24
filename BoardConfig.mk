982b5297e8d62259bc19a0b68fb93249c3f59a65
Michael Sky <electrydev@gmail.com>
2016-11-24 18:41:36 +0100

    kingdom_row: Initial minimal TWRP tree
    * Based off cm-14.1 tree
    
    Signed-off-by: Michael Sky <electrydev@gmail.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,91 @@
+#
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+DEVICE_PATH := device/lenovo/kingdom_row
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := MSM8974
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+
+# Platform
+TARGET_BOARD_PLATFORM := msm8974
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno330
+
+# Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := krait
+TARGET_CPU_SMP := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
+
+# Kernel
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom \
+			androidboot.bootdevice=msm_sdcc.1 \
+			ehci-hcd.park=3 \
+			androidboot.selinux=permissive
+BOARD_KERNEL_SEPARATED_DT := true
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x01e00000
+BOARD_DTBTOOL_ARGS := -2
+TARGET_KERNEL_ARCH := arm
+TARGET_KERNEL_SOURCE := kernel/lenovo/msm8974
+TARGET_KERNEL_CONFIG := kingdom_row_defconfig
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
+
+# Encryption
+TARGET_HW_DISK_ENCRYPTION := true
+
+# Filesystem
+BOARD_FLASH_BLOCK_SIZE                  := 131072
+BOARD_BOOTIMAGE_PARTITION_SIZE          := 20971520
+BOARD_RECOVERYIMAGE_PARTITION_SIZE      := 20971520
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE       := ext4
+BOARD_CACHEIMAGE_PARTITION_SIZE         := 134217728
+BOARD_PERSISTIMAGE_FILE_SYSTEM_TYPE     := ext4
+BOARD_PERSISTIMAGE_PARTITION_SIZE       := 33554432
+BOARD_SYSTEMIMAGE_PARTITION_SIZE        := 2147483648
+BOARD_USERDATAIMAGE_PARTITION_SIZE      := 28097608704 # 28097625088 - 16384
+BOARD_OEMIMAGE_FILE_SYSTEM_TYPE         := ext4
+BOARD_OEMIMAGE_PARTITION_SIZE           := 134217728
+
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+
+# Keymaster
+TARGET_KEYMASTER_WAIT_FOR_QSEE := true
+
+# Recovery (TWRP)
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/fstab.twrp
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+DEVICE_RESOLUTION := 1440x2560
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+RECOVERY_VARIANT := twrp
+RECOVERY_SDCARD_ON_DATA := true
+BOARD_HAS_NO_REAL_SDCARD := true
+TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_FB2PNG := true
+TW_MAX_BRIGHTNESS := 255
+TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+
+# SELinux
+include device/qcom/sepolicy/sepolicy.mk
+
+BOARD_SEPOLICY_DIRS += \
+    $(DEVICE_PATH)/sepolicy
---
+++ vendorsetup.sh
@@ -0,0 +1,1 @@
+add_lunch_combo cm_kingdom_row-eng
