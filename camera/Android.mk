4c17395313278e6d312a5dcf335edfde28e53c23
Michal Chvila <electrydev@gmail.com>
2017-03-22 17:33:48 +0100

    kingdom: Import OSS camera HAL LA.BF.1.1.1-02210-8x74.0
    Signed-off-by: Michal Chvila <electrydev@gmail.com>

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,34 +1,14 @@
-#
-# Copyright 2017 The LineageOS Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+MM_V4L2_DRIVER_LIST += msm8960
+MM_V4L2_DRIVER_LIST += msm8974
+MM_V4L2_DRIVER_LIST += msm8226
+MM_V4L2_DRIVER_LIST += msm8610
+MM_V4L2_DRIVER_LIST += msm_bronze
+MM_V4L2_DRIVER_LIST += msm8916
 
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := \
-    CameraWrapper.cpp
-
-LOCAL_C_INCLUDES := \
-    system/media/camera/include
-
-LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libutils libcutils libdl
-
-LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/hw
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE := camera.msm8974
-
-include $(BUILD_SHARED_LIBRARY)
+ifeq ($(call is-board-platform-in-list,$(MM_V4L2_DRIVER_LIST)),true)
+  ifneq ($(USE_CAMERA_STUB),true)
+    ifneq ($(BUILD_TINY_ANDROID),true)
+      include $(call all-subdir-makefiles)
+    endif
+  endif
+endif
