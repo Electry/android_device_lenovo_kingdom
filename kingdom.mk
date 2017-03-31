16bf8b48ce74c46cd27472dfa252224cc79d0889
Myles Watson <mylesgw@google.com>
2017-03-31 11:25:04 -0700

    kingdom: Add libbt-vendor to kingdom.mk
    Bug: 36810308
    Test: build
    Change-Id: Iae4a6340a184dde818603b546470c8cf883f2f7b
    Signed-off-by: Michal Chvila <electrydev@gmail.com>

--- kingdom.mk
+++ kingdom.mk
@@ -64,7 +64,8 @@ PRODUCT_PACKAGES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-service
+    android.hardware.bluetooth@1.0-service \
+    libbt-vendor
 
 # Boot animation
 TARGET_BOOTANIMATION_HALF_RES := true
