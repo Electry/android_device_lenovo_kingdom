ee942c7eedb1f0161cc26cf2bc4e4cc5aea84cd0
Michal Chvila <electrydev@gmail.com>
2017-05-19 12:33:15 +0200

    kingdom: Do not inherit common stuff

--- lineage.mk
+++ lineage.mk
@@ -13,12 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
 PRODUCT_NAME := lineage_kingdom
 PRODUCT_DEVICE := kingdom
 PRODUCT_BRAND := Lenovo
