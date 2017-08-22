8f75a811c39f968e288c1c345d2ffd82918f4e1f
Simon Shields <simon@lineageos.org>
2017-08-22 16:32:00 +0200

    kingdom: vendor/cm -> vendor/lineage
    Change-Id: I13a505be438844a3ed4e340a0e27047e1cfa4688
    Signed-off-by: Michal Chvila <electrydev@gmail.com>

--- lineage.mk
+++ lineage.mk
@@ -20,7 +20,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/lenovo/kingdom/kingdom.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_kingdom
 PRODUCT_DEVICE := kingdom
