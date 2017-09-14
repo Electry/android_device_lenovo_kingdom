b49cb585b289fff42c9ed2879dc36155137a4c60
Ethan Chen <intervigil@gmail.com>
2017-09-14 01:01:28 -0700

    kingdom: Add seccomp policy
    Change-Id: I1d5017228a4a9318f8a77ef4036532d64718ecbb

--- kingdom.mk
+++ kingdom.mk
@@ -271,6 +271,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
 
+# Seccomp
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/seccomp/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl
