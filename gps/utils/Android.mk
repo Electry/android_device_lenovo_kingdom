528229643f2de1470b50d5fed3050e28f6032e9d
Michael Bestas <mkbestas@lineageos.org>
2017-09-27 23:36:53 +0300

    kingdom: gps: Cleanup makefiles and remove useless files
    Change-Id: I66848d2e220817ae61642d7211f7eabf5e4efcea

--- gps/utils/Android.mk
+++ gps/utils/Android.mk
@@ -1,6 +1,4 @@
 #ifneq ($(BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE),)
-ifneq ($(BUILD_TINY_ANDROID),true)
-#Compile this library only for builds with the latest modem image
 
 LOCAL_PATH := $(call my-dir)
 
@@ -63,8 +61,6 @@ LOCAL_MODULE := libgps.utils
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_PRELINK_MODULE := false
-
 include $(BUILD_SHARED_LIBRARY)
-endif # not BUILD_TINY_ANDROID
+
 #endif # BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE
