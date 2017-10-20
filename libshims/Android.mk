4e8acd8181b2040feee22a0c30b7b6baf5ca100b
jrior001 <jriordan001@gmail.com>
2017-10-19 23:00:45 -0400

    kingdom: Convert CameraParameters header to shim lib
    Change-Id: I31c7c3346c8fac4020cd86822c9cfb1a6db26b77

---
+++ libshims/Android.mk
@@ -0,0 +1,14 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    CameraParameters.cpp
+
+LOCAL_MODULE := libshim_camera_parameters
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_32_BIT_ONLY := true
+
+include $(BUILD_SHARED_LIBRARY)
