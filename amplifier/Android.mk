ccad99f3a084072ef00c91c945721d6b6994cb13
Bruno Martins <bgcngm@gmail.com>
2017-10-25 17:40:48 +0100

    kingdom: amplifier: Move module to vendor
    * Make use of $(TARGET_BOARD_PLATFORM) while at it
    
    Change-Id: Ie08fae36f1d2bb0112cf6239f412777fa2762a37

--- amplifier/Android.mk
+++ amplifier/Android.mk
@@ -1,5 +1,5 @@
 #
-# Copyright 2017 The LineageOS Project
+# Copyright 2017-2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -34,6 +34,7 @@ LOCAL_SHARED_LIBRARIES := \
 
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE := audio_amplifier.msm8974
+LOCAL_MODULE := audio_amplifier.$(TARGET_BOARD_PLATFORM)
+LOCAL_PROPRIETARY_MODULE := true
 
 include $(BUILD_SHARED_LIBRARY)
