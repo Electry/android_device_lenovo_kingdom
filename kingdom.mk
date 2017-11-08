92ac53ed921d8a8a4cf11762a0f80e99f6a0066f
Michal Chvila <electrydev@gmail.com>
2017-11-08 18:04:14 +0100

    kingdom: nfc_nci.pn54x.default -> nfc_nci.msm8974
    Change-Id: If9efc2a943ca7c2fdbb07f0ae1b685d3c5e6fc16
    Signed-off-by: Michal Chvila <electrydev@gmail.com>

--- kingdom.mk
+++ kingdom.mk
@@ -229,7 +229,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.0-impl \
-    nfc_nci.pn54x.default \
+    nfc_nci.msm8974 \
     com.android.nfc_extras \
     NfcNci \
     Tag
