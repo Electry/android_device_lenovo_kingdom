9f489494dd8e1fe49092f8cf44352840298aad2a
Michal Chvila <electrydev@gmail.com>
2017-11-10 12:51:03 +0100

    kingdom: Move thermal-engine config to vendor
    * Requires hexing thermal-engine binary:
    sed -i "s|/system/etc/|/vendor/etc/|g" vendor/bin/thermal-engine
    
    Change-Id: Ib8a18338d0a6d6400869d1d63592f741d3f2a48a
    Signed-off-by: Michal Chvila <electrydev@gmail.com>

--- kingdom.mk
+++ kingdom.mk
@@ -285,7 +285,7 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal-engine.conf:system/etc/thermal-engine-8974.conf
+    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-8974.conf
 
 # USB
 PRODUCT_PACKAGES += \
