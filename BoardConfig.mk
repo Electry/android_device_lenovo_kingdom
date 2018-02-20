37230024c501c1b7c9e3af87ff7c5fe8c52939ee
Michal Chvila <michal@chvila.sk>
2018-02-20 11:06:13 +0100

    kingdom: Disable EAP Proxy
    Change-Id: Ic2d2fb7692207b7e7352f95bec08a2d0d9239f2c
    Signed-off-by: Michal Chvila <michal@chvila.sk>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -203,8 +203,8 @@ TARGET_USES_QCOM_WCNSS_QMI       := true
 TARGET_USES_WCNSS_MAC_ADDR_REV   := true
 
 # Wifi - EAP-SIM
-CONFIG_EAP_PROXY                 := qmi
-CONFIG_EAP_PROXY_DUAL_SIM        := true
+#CONFIG_EAP_PROXY                 := qmi
+#CONFIG_EAP_PROXY_DUAL_SIM        := true
 
 # DEX Pre-optimization
 ifeq ($(HOST_OS),linux)
