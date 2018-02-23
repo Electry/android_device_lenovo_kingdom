f1131f5889e845c888d52c2def67bc9ddbdcac95
Michal Chvila <michal@chvila.sk>
2018-02-23 15:26:10 +0100

    kingdom: Remove custom BOARD_CHARGING_CMDLINE_VALUE
    * not supported anymore, let's do this in kernel instead
    
    Change-Id: I31dbce723104623585d4acc9636baa518c6a126d
    Signed-off-by: Michal Chvila <michal@chvila.sk>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -85,8 +85,6 @@ USE_DEVICE_SPECIFIC_CAMERA := true
 # Charger
 BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_CHARGER_DISABLE_INIT_BLANK := true
-BOARD_CHARGING_CMDLINE_NAME := "androidboot.mode"
-BOARD_CHARGING_CMDLINE_VALUE := "usb_cable"
 BACKLIGHT_PATH := "/sys/class/leds/lcd-backlight/brightness"
 RED_LED_PATH := "/sys/class/leds/led:rgb_red/brightness"
 GREEN_LED_PATH := "/sys/class/leds/led:rgb_green/brightness"
