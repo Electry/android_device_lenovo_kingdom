93b7c9023b561aa55912d29f7484c8a643e6ff2f
Michal Chvila <michal@chvila.sk>
2018-02-24 00:40:20 +0100

    kingdom: vendor/cm -> vendor/lineage
    Change-Id: I1210d6773884dd72e58f60b0d64173cf3b8542f6
    Signed-off-by: Michal Chvila <michal@chvila.sk>

--- lineage.mk
+++ lineage.mk
@@ -16,8 +16,8 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_kingdom
 PRODUCT_DEVICE := kingdom
